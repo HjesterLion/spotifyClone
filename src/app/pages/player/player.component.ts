393026dd9b3475b6d54708f8ca068f1ed2f333fd
HjesterLion <lucasnakazima@outlook.com.br>
2023-05-12 11:58:44 -0300

    Corrigido erro de interface

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyServicesService } from 'src/app/services/spotify-services.service';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
+
+  constructor(private spotifyService: SpotifyServicesService) { }
+
+  ngOnInit(): void {
+    this.menuSelecionado()
+
+  }
+  menuSelecionado() {
+    const endPoint = window.location.href.split('/').pop()
+    if (endPoint == 'home') {
+      this.spotifyService.menuSelecionado = 'Home'
+      return
+    }
+    if (endPoint == 'pesquisar') {
+      this.spotifyService.menuSelecionado = 'Pesquisar'
+      return
+    }
+    if (endPoint == 'artistas') {
+      this.spotifyService.menuSelecionado = 'Artistas'
+      return
+    }
+    this.spotifyService.menuSelecionado = endPoint
+
+  }
 
 }
